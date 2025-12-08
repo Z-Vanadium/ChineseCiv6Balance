d52f0df44d27cf4449f974a8373ec9a38841178a
z-vanadium <105497687@qq.com>
2025-12-08 15:07:41 +0800

    unit at crew and modern at +1 movement

--- sql/Base/Units.sql
+++ sql/Base/Units.sql
@@ -40,7 +40,8 @@ UPDATE Units SET BaseMoves=5 WHERE UnitType='UNIT_AIRCRAFT_CARRIER';
 UPDATE Units SET Range=4 WHERE UnitType='UNIT_MISSILE_CRUISER';
 
 -- 31/07/2021 Late Game Unit rework
-UPDATE Units SET Combat=80 WHERE UnitType='UNIT_AT_CREW';
+-- 2025/12/08 at_crew and modern at +1 movement
+UPDATE Units SET Combat=80, BaseMoves=3 WHERE UnitType='UNIT_AT_CREW';
 UPDATE Units SET Combat=80, BaseMoves=3 WHERE UnitType='UNIT_INFANTRY';
 UPDATE Units SET Combat=65, RangedCombat=75 WHERE UnitType='UNIT_BATTLESHIP';
 -- 03/10/22: movement from 6 to 5
@@ -52,7 +53,7 @@ UPDATE Units SET BaseMoves=3 WHERE UnitType='UNIT_MACHINE_GUN';
 UPDATE Units SET Combat=80 WHERE UnitType='UNIT_AIRCRAFT_CARRIER';
 UPDATE Units SET Combat=90 WHERE UnitType='UNIT_DESTROYER';
 -- UPDATE Units SET Combat=75 WHERE UnitType='UNIT_ROCKET_ARTILLERY';
-UPDATE Units SET Combat=90 WHERE UnitType='UNIT_MODERN_AT';
+UPDATE Units SET Combat=90, BaseMoves=4 WHERE UnitType='UNIT_MODERN_AT';
 UPDATE Units SET Combat=90 WHERE UnitType='UNIT_MECHANIZED_INFANTRY';
 -- UPDATE Units SET Combat=85, RangedCombat=95 WHERE UnitType='UNIT_NUCLEAR_SUBMARINE';
 -- UPDATE Units SET Combat=80, RangedCombat=95 WHERE UnitType='UNIT_MISSILE_CRUISER';
