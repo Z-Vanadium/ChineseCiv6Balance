c38ac861510418cf8d17a6bf73dde42bc2da5398
z-vanadium <105497687@qq.com>
2025-12-08 17:56:22 +0800

    Egypt 5 golds per international traderoutes instead of 4

--- sql/Base/Egypt.sql
+++ sql/Base/Egypt.sql
@@ -97,7 +97,8 @@ INSERT OR IGNORE INTO RequirementSetRequirements (RequirementSetId , Requirement
 
 --14/07/2022: Egypt 6 golds per international traderoutes instead of 4
 -- 02/12/24 reverted
--- UPDATE ModifierArguments SET Value='6' WHERE ModifierId='TRAIT_INTERNATIONAL_TRADE_GAIN_GOLD' AND Name='Amount';
+-- 2025/12/08 changed to 5 golds
+UPDATE ModifierArguments SET Value='5' WHERE ModifierId='TRAIT_INTERNATIONAL_TRADE_GAIN_GOLD' AND Name='Amount';
 -- 02/12/24 food reduced to 1
 UPDATE ModifierArguments SET Value=1 WHERE ModifierId='TRAIT_INCOMING_TRADE_OFFER_FOOD' AND Name='Amount';
 
