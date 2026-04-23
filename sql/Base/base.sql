9d16468135d42e226d158df30ff5327f52757aae
vanadium_zz <105497687@qq.com>
2026-04-23 19:20:42 +0800

    nuclear fusion do not require guidance systems fix

--- sql/Base/base.sql
+++ sql/Base/base.sql
@@ -357,14 +357,9 @@ INSERT INTO TechnologyPrereqs (Technology, PrereqTech) VALUES
     ('TECH_ROBOTICS', 'TECH_TELECOMMUNICATIONS');
 
 -- 2026/04/34: nuclear fusion do not require guidance systems
-DELETE FROM TechnologyPrereqs
-      WHERE Technology = 'TECH_NUCLEAR_FUSION' AND
-            PrereqTech = 'TECH_GUIDANCE_SYSTEMS';
-
 -- 09/04/25 Guidance as prereq for Nanotech and Nuclear Fusion (swapped lasers and guidance on the tech tree so it makes more sense)
 INSERT INTO TechnologyPrereqs (Technology, PrereqTech) VALUES
-    ('TECH_NANOTECHNOLOGY', 'TECH_GUIDANCE_SYSTEMS'),
-    ('TECH_NUCLEAR_FUSION', 'TECH_GUIDANCE_SYSTEMS');
+    ('TECH_NANOTECHNOLOGY', 'TECH_GUIDANCE_SYSTEMS');
 UPDATE Technologies SET UITreeRow=0 WHERE TechnologyType='TECH_LASERS';
 UPDATE Technologies SET UITreeRow=1 WHERE TechnologyType='TECH_GUIDANCE_SYSTEMS';
 
