178f9579497644d3acce6fc5108031045376ec1a
vanadium_zz <105497687@qq.com>
2026-06-30 16:27:59 +0800

    北条：浅水海军战斗力 +5

--- sql/Base/Japan.sql
+++ sql/Base/Japan.sql
@@ -8,7 +8,8 @@ INSERT OR IGNORE INTO ExcludedAdjacencies (TraitType , YieldChangeId) VALUES
 UPDATE Units SET PrereqCivic='CIVIC_MERCENARIES', PrereqTech=NULL WHERE UnitType='UNIT_JAPANESE_SAMURAI';
 
 -- 02/07/24 Nerf combat strenght from +5  to +3
-UPDATE ModifierArguments SET Value = '3' WHERE ModifierId = 'HOJO_TOKIMUNE_SHALLOW_WATER_COMBAT_BONUS' AND Name = 'Amount';
+-- 26/06/39 浅水海军战斗力 +5
+-- UPDATE ModifierArguments SET Value = '3' WHERE ModifierId = 'HOJO_TOKIMUNE_SHALLOW_WATER_COMBAT_BONUS' AND Name = 'Amount';
 UPDATE ModifierArguments SET Value = '3' WHERE ModifierId = 'HOJO_TOKIMUNE_COASTAL_COMBAT_BONUS' AND Name = 'Amount';
 
 --26/02/25 Culture from electronic factory is now base bonus of the building
