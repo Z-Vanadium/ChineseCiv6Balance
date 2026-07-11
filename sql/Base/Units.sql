c9c0a1f9c48b418f17bd35c1bc0c602031a6933a
vanadium_zz <105497687@qq.com>
2026-07-12 01:01:32 +0800

    野战加农炮不再过时

--- sql/Base/Units.sql
+++ sql/Base/Units.sql
@@ -149,7 +149,8 @@ UPDATE Units SET AntiAirCombat=125, BaseMoves=5 WHERE UnitType='UNIT_MOBILE_SAM'
 -- 16/12/22 Obsolescence
 -- 15/10/23 Added Varus
 -- 30/03/25 Units are now obsolete when the next tech is unlocked 05/04/25 reverted
--- 2026/07/12 抗骑兵及其UU不再过时
+-- 2026/07/12 抗骑兵及其UU不再过时；野战加农炮不再过时
+UPDATE Units SET MandatoryObsoleteTech=NULL WHERE UnitType='UNIT_FIELD_CANNON';
 UPDATE Units SET MandatoryObsoleteTech=NULL WHERE UnitType IN ('UNIT_SPEARMAN', 'UNIT_GREEK_HOPLITE', 'UNIT_PIKEMAN', 'UNIT_CVS_SWITZERLAND_UU', 'UNIT_ZULU_IMPI', 'UNIT_PIKE_AND_SHOT', 'UNIT_SWEDEN_CAROLEAN');
 UPDATE Units SET MandatoryObsoleteTech='TECH_MILITARY_TACTICS' WHERE UnitType='UNIT_WARRIOR';
 UPDATE Units SET MandatoryObsoleteTech='TECH_BALLISTICS' WHERE UnitType='UNIT_HEAVY_CHARIOT';
