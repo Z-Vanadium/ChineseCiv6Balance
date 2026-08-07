df43d8da121be2b843effac233e033b6672dad86
vanadium_zz <105497687@qq.com>
2026-08-07 21:03:37 +0800

    revert 现代大将军：对信息时代单位生效（而不是仅到原子能时代）

--- sql/Base/GreatPeoples.sql
+++ sql/Base/GreatPeoples.sql
@@ -216,8 +216,3 @@ UPDATE GreatPersonIndividuals SET ActionEffectTileHighlighting=1 WHERE GreatPers
 --10/03/2024 zhou dagan can be activated in hostile territory
 
 UPDATE GreatPersonIndividuals SET ActionRequiresNonHostileTerritory=0 WHERE GreatPersonIndividualType='GREAT_PERSON_INDIVIDUAL_ZHOU_DAGUAN';
-
--- 2026/07/12 现代大将军：对信息时代单位生效（而不是仅到原子能时代）
--- LOC_GREATPERSON_COMBAT_STRENGTH_AOE_MODERN_LAND
-INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId) VALUES
-    ('AOE_MODERN_REQUIREMENTS', 'AOE_REQUIRES_INFORMATION_UNIT');
